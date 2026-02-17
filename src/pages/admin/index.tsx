b5f2d51cdb41a1d4af19f8dceb066559c805685b
mrkadirov2005 <muzaffar571181@gmail.com>
2026-02-17 19:24:32 +0500

    setup the development environment

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,9 @@
+import { AdminInvitesComponent } from './components/invite/AdminInvites'
+
+export default function AdminPage() {
+  return (
+    <>
+    <AdminInvitesComponent/>
+    </>
+  )
+}
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,7 @@
+import { LoginComponent } from "./components/Login";
+
+export default function LoginPage() {
+    return (
+        <><LoginComponent /></>
+    )
+}
