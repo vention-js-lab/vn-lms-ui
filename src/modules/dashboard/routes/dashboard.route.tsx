79dad7faa5d5b6b661facc7e8aa1917a526cd20a
mrkadirov2005 <muzaffar571181@gmail.com>
2026-03-13 04:37:25 +0500

    [PANTHERS-36] Added PR template

--- src/modules/dashboard/routes/dashboard.route.tsx
+++ src/modules/dashboard/routes/dashboard.route.tsx
@@ -1,7 +1,7 @@
 export function DashboardRoute() {
   return (
     <section className="flex min-h-screen items-center justify-center">
-      <h1 className="p-2 text-3xl font-bold">Dashboard</h1>
+      <h1 className="text-3xl font-bold">Dashboard</h1>
     </section>
   );
 }
